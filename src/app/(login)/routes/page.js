ab5364d397daaa5f399116c9a433006765b8330b
Hanideepu-Kodi <theuncrownedking44@gmail.com>
2025-11-16 19:28:09 +0800

    pages skeleton + navbabar

---
+++ src/app/(login)/routes/page.js
@@ -0,0 +1,7 @@
+export default function RoutesPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <h1 className="text-2xl font-semibold">This is the Routes page</h1>
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { Typewriter } from "@/components/ui/typewriter-text"
+import Link from "next/link";
+import { Typewriter } from "@/components/ui/typewriter-text";
 
 export default function Home() {
   return (
@@ -14,8 +15,8 @@ export default function Home() {
                 loop={true}
                 className="text-xl font-medium"
         />
-          {/* Google */}
-          <button className="w-full flex items-center justify-center gap-x-3 py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
+          {/* Login Button: Need to change this to proper Authentication if the backend completes */}
+          <Link href="/home" className="w-full flex items-center justify-center gap-x-3 py-2.5 border rounded-lg hover:bg-gray-50 duration-150 active:bg-gray-100">
             <svg
               className="w-5 h-5"
               viewBox="0 0 533.5 544.3"
@@ -39,7 +40,7 @@ export default function Home() {
               />
             </svg>
             Continue with Google
-          </button>
+          </Link>
       </main>
     </div>
   );
